e307a0bf04ae499f6ea87611f4b9fc4161ae9394
Q <q@misell.cymru>
2023-10-24 23:09:32 +0100

    cockroach migration fixes, take 2

--- server/db/migrations/2.2.17.js
+++ server/db/migrations/2.2.17.js
@@ -6,6 +6,7 @@ exports.up = async knex => {
   let sqlVersionDate = ''
   switch (WIKI.config.db.type) {
     case 'postgres':
+    case 'cockroach':
       sqlVersionDate = 'UPDATE "pageHistory" h1 SET "versionDate" = COALESCE((SELECT prev."createdAt" FROM "pageHistory" prev WHERE prev."pageId" = h1."pageId" AND prev.id < h1.id ORDER BY prev.id DESC LIMIT 1), h1."createdAt")'
       break
     case 'mssql':
